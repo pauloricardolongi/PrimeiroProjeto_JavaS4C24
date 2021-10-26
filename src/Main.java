ae7c7f5fc21029447302911d8c3df05c9068e018
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-26 04:03:37 -0300

    concatenando varios elementos

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		int y=32;
-		double x= 10.35784;
-		System.out.println(y);
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);//virgula português
-		System.out.printf("%.4f%n", x);
+		/*concatenar vários elementos:
+		 %f= ponto flutuante - 4000.0
+		 %d= inteiro- 31 anos
+		 %s = texto - Maria
+		 %n= quebra de linha
+		 */
+		String nome= "Maria";
+		int idade= 31;
+		double renda= 4000.0;
 		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n", x);//ingles
-		System.out.println("resultado = " + x + " metros");//concatenar
-		System.out.printf("resultado = %.2f metros %n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais.%n",nome, idade, renda);
+		
 		
 		
 		
