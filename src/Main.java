12f01ce7eebaf4df862388d499c35f7c7ac980d9
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-27 02:37:35 -0300

    cunsumir a quebra de linha pendente

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		int x;
 		String s1, s2, s3;
 		
-		s1 = sc.nextLine();//quebra de linha
+		x= sc.nextInt();
+		sc.nextLine();//consumir quebra de linha pendente
+		s1 = sc.nextLine();
 		s2 = sc.nextLine();
 		s3 = sc.nextLine();
 
 		System.out.println("DADOS DIGITADOS:");
+		System.out.println(x);
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
