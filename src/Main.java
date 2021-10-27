b22b2a62d3a390d99ca6e02349e0b2115d95e296
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-27 00:51:12 -0300

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
-		/*int a, b;
-		double resultado;
-		a=5;
-		b=2;
-		resultado= (double)a/b;
-		System.out.println(resultado);//questi*/
-		double a;
-		int b;
-		a=5.0;
-		b= (int)a;
-		System.out.print(b);
+	//entrada de dados SCANNER 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-				
+		double x;
+		x= sc.nextDouble();
+		System.out.printf("Você digitou: %.2f%n",  x);
+		
+		sc.close();
 		
 	}
 
