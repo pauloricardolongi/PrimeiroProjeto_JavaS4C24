1b090f6410ab3f5304a840c5b53fe821b5ddca63
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-26 23:39:43 -0300

    Casting

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		//base de um trapesio
-		double b, B, h, area;
-		b=6.0;
-		B=8.0;
-		h=5.0;
-		area= (b + B)/ 2.0 *h;
-		System.out.println(area);
+		/*int a, b;
+		double resultado;
+		a=5;
+		b=2;
+		resultado= (double)a/b;
+		System.out.println(resultado);//questi*/
+		double a;
+		int b;
+		a=5.0;
+		b= (int)a;
+		System.out.print(b);
+		
+				
 		
 	}
 
