8c247b72146621b8242ab972b9378b372e55cdb5
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-27 03:19:25 -0300

    Raiz quadrada, Potenciação e Valor absoluto

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
-import java.util.Scanner;
+
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int x;
-		String s1, s2, s3;
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
 		
-		x= sc.nextInt();
-		sc.nextLine();//consumir quebra de linha pendente
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
-
-		System.out.println("DADOS DIGITADOS:");
-		System.out.println(x);
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		sc.close();
+		A = Math.sqrt(x);//raiz quadrada
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadrado = " + C);
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
 	}
 }
