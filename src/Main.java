9d1c45043faf0ea65e47b5d0d08f03de96e1fbdd
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-27 01:18:44 -0300

    entrada de vários dados

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	//ler um caracter
+	
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		char x;
-		x= sc.next().charAt(0);
-		System.out.println("Você digitou:" +  x);
+		String x;
+		int y;
+		double z;
+		x= sc.next(); //string
+		y= sc.nextInt();
+		z= sc.nextDouble();
+		System.out.println("Dados digitados:");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		
 		sc.close();
 		
