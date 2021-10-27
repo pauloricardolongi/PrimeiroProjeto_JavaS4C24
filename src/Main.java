c298c25e1fddd8e23357b5b6259602a5cf39d2cc
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-27 02:08:05 -0300

    nextLine para ler a linha inteira e quebra linha

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-	
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		String x;
-		int y;
-		double z;
-		x= sc.next(); //string
-		y= sc.nextInt();
-		z= sc.nextDouble();
-		System.out.println("Dados digitados:");
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		sc.close();
-		
-	}
 
+		String s1, s2, s3;
+		
+		s1 = sc.nextLine();//quebra de linha
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		sc.close();
+	}
 }
