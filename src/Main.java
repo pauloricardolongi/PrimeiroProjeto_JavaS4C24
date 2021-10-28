751e9e4196a82ae9c9622617604a94ec7d00c1d0
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-28 02:39:42 -0300

    dez e cinco por cento

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,13 @@
-import java.util.Locale;
+
+
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		/*
-		 * Fazer um programa para ler um valor inteiro de 1 a 7 representando um dia da
-		 * semana (sendo 1= domingo, 2= segunda, e assim por diante)
-		 */
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		String dia;
-		switch (x) {
-		case 1:
-			dia = "domingo";
-			break;
-		case 2:
-			dia = "segunda";
-			break;
-		case 3:
-			dia = "terca";
-			break;
-		case 4:
-			dia = "quarta";
-			break;
-		case 5:
-			dia = "quinta";
-			break;
-		case 6:
-			dia = "sexta";
-			break;
-		case 7:
-			dia = "sabado";
-			break;
-		default:
-			dia = "valor invalido";
-			break;
-		}
-		System.out.println("Dia da semana: " + dia);
+		Scanner sc= new Scanner(System.in);
+		double preco = 100;
+		double desconto = (preco <= 100.0) ? preco * 0.1 : preco * 0.05;
+		System.out.println(desconto);
 		sc.close();
 	}
 }
