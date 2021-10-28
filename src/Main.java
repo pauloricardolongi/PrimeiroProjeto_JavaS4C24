14a4dc3129afbf6756cbc08a659dd711cb68c60c
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-28 01:41:00 -0300

    programa dia da semana if else

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,36 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		/* Problema exemplo:
-		 Uma operadora de telefonia cobra R$50,00 por um plano básico que dá direito a 100 minutos de telefone.
-		 Cada minuto que exceder  a franquia de 100 minutos custa R$2,00.Fazer um programa para ler a quantidade
-		 de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
-		 ENTRADA                                     SAÍDA
-		 22MINUTOS                                 VALOR A PAGAR R$50,00
-		 103 MINUTOS                               VALOR A PAGAR R$ R$56,00*/
-		Locale.setDefault(Locale.US);
-		Scanner sc= new Scanner(System.in);
-		
-		int minutos= sc.nextInt(); //quantidade de minutos consumido
-		
-		double conta = 50.0; //valor da conta
-		if(minutos > 100) {
-			conta = conta + (minutos - 100)* 2.0; //calcular o excedente e multiplicar por 2.0
+		/* Fazer um programa para ler um valor inteiro de 1 a 7 representando um dia da semana
+		 (sendo 1= domingo,  2= segunda, e assim por diante)*/
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		String dia;
+		if (x == 1) {
+		dia = "domingo";
 		}
-		 System.out.printf("Valor da conta = R$ %.2f%n",conta );
-		
+		else if (x == 2) {
+		dia = "segunda";
+		}
+		else if (x == 3) {
+		dia = "terca";
+		}
+		else if (x == 4) {
+		dia = "quarta";
+		}
+		else if (x == 5) {
+		dia = "quinta";
+		}
+		else if (x == 6) {
+		dia = "sexta";
+		}
+		else if (x == 7) {
+		dia = "sabado";
+		}
+		else {
+		dia = "valor invalido";
+		}
+		System.out.println("Dia da semana: " + dia);
 		sc.close();
-	}
+		}
 }
