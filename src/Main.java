575d6510ae061879c1032eef1056ae43952b1be3
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-28 01:03:57 -0300

    problema 01- conta de telefone

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		/* Problema exemplo:
+		 Uma operadora de telefonia cobra R$50,00 por um plano básico que dá direito a 100 minutos de telefone.
+		 Cada minuto que exceder  a franquia de 100 minutos custa R$2,00.Fazer um programa para ler a quantidade
+		 de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
+		 ENTRADA                                     SAÍDA
+		 22MINUTOS                                 VALOR A PAGAR R$50,00
+		 103 MINUTOS                               VALOR A PAGAR R$ R$56,00*/
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
 		
-		Scanner sc = new Scanner(System.in);		
-		int hora;
+		int minutos= sc.nextInt(); //quantidade de minutos consumido
 		
-		System.out.println("Qantas horas?");
-		hora = sc.nextInt();
-		
-		if (hora < 12 ) {
-			System.out.println("Bom dia");	
-		
+		double conta = 50.0; //valor da conta
+		if(minutos > 100) {
+			conta = conta + (minutos - 100)* 2.0; //calcular o excedente e multiplicar por 2.0
 		}
-		else if(hora < 18) {
-			System.out.println("Boa tarde");				
-			}
-		else  {
-			System.out.println("Boa noite");
-			}
-		
+		 System.out.printf("Valor da conta = R$ %.2f%n",conta );
 		
 		sc.close();
 	}
