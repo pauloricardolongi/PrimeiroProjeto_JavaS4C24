258b89da4fa4fe810c1a7a8098f550a98f7bf18b
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-30 06:18:15 -0300

    while-enquanto diferente de zero soma, 0 pula fora

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		/* while - enquanto for verdadeiro pula fora
-		  e volta
-		 */
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int x = sc.nextInt();
+		int soma = 0;
 		
 		while (x !=0) {
+			soma = soma + x; /* exemplo: soma= 0 + 5-> soma =5
+			4 e 0 total 9 */
 			x = sc.nextInt();
 		}
+		System.out.println(soma);
 		sc.close();
 	}
 }
