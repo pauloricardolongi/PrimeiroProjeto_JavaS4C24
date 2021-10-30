308a04033df4138c640b01cd87052ba43359a1ea
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-30 06:05:24 -0300

    while - enquanto for verdadeiro pula fora   e volta,

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
-import java.util.Locale;
+
 import java.util.Scanner;
 
 public class Main {
-	/* */
+	
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
+		/* while - enquanto for verdadeiro pula fora
+		  e volta
+		 */
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("digite a largura do terreno");
-		double largura = sc.nextDouble();
-		System.out.println("digite o comprimento do terreno");
-		double comprimento = sc.nextDouble();
-		System.out.println("digite o valor do metro quadrado");
-		double metroQuadrado = sc.nextDouble();
+		int x = sc.nextInt();
 		
-		double area = largura * comprimento;
-		double preco = area * metroQuadrado;
-		
-		System.out.printf("AREA = %.2f%n", area);
-		System.out.printf("PRECO TOTAL = %.2f%n", preco);
+		while (x !=0) {
+			x = sc.nextInt();
+		}
 		sc.close();
 	}
 }
