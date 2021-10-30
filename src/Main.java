99d5d9a4079488a97694374226bd6d584e60e710
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-30 05:46:59 -0300

    seção 06, continuação area do terreno

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,23 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+	/* */
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("digite a largura do terreno");
 		double largura = sc.nextDouble();
+		System.out.println("digite o comprimento do terreno");
 		double comprimento = sc.nextDouble();
+		System.out.println("digite o valor do metro quadrado");
 		double metroQuadrado = sc.nextDouble();
 		
 		double area = largura * comprimento;
 		double preco = area * metroQuadrado;
 		
 		System.out.printf("AREA = %.2f%n", area);
-		System.out.printf("PRECO = %.2f%n", preco);
+		System.out.printf("PRECO TOTAL = %.2f%n", preco);
 		sc.close();
 	}
 }
