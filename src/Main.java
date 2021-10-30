6a72a7712ceac9c7513cf60495317d6daffa0960
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-30 05:33:42 -0300

    calculo largura, comprimento, valor metroQuadrado, e preço total

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-
-
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		double preco = 100;
-		double desconto = (preco <= 100.0) ? preco * 0.1 : preco * 0.05;
-		System.out.println(desconto);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
 		sc.close();
 	}
 }
